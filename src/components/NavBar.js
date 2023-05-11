57b0c7cc79ae3f9ef9dab2b71ca5f9723ae6a2bb
Matthew Ishida <matthew.ishida@gmail.com>
2023-05-10 18:08:56 -1000

    add images to modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,10 +16,38 @@ function NavBar() {
             </Navbar>
 
             <Modal show={show} onHide={handleClose}>
-                <Modal.Body>Hover over vote tiles to see additional info</Modal.Body>
-                <Modal.Body>Click "Show similar votes" to see who else voted the same way</Modal.Body>
-                <Modal.Body>Built by Matt Ishida</Modal.Body>
-                <Modal.Body>Data is refreshed daily from the ProPublica Congress API</Modal.Body>
+                <Modal.Body>
+                    <div>
+                        <strong>What is VoteTracker?</strong>
+                        <p>A simple web app for visualizing votes in the US congress</p>
+                    </div>
+                    <div>
+                        <strong>How do I use it?</strong>
+                        <ul>
+                            <li>"Yes" votes are green; "No" votes are red</li>
+                            <li>Gray/white shading indicates consecutive votes on the same bill</li>
+                            <li>Hover over each vote to see more details
+                                <div style={{textAlign: 'center'}}>
+                                    <img src="/votetracker_hover.jpg" alt="an example of detailed vote info" height="300px"/>
+                                </div>
+
+                            </li>
+                            <li>Click "Show similar votes" to see who voted the same way</li>
+                            <li>Use "Highlight by Alignment" to highlight atypical votes
+                                <div style={{textAlign: 'center'}}>
+                                    <img src="/votetracker_highlight.jpg" alt="an example of highlighted votes" height="50px"/>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <strong>Credits</strong>
+                        <ul>
+                            <li>Data is refreshed daily from the <a href="https://projects.propublica.org/api-docs/congress-api" target="_blank">ProPublica Congress API</a></li>
+                            <li>App by <a href="https://github.com/mattIshida" target="_blank">@mattIshida</a></li>
+                        </ul>
+                    </div>
+                </Modal.Body>
             </Modal>
         </>
     )
